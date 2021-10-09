c9cead4122ec9acaa77f5f7c0eb6b45de33eebf7
Wei Lun <weilun_95@hotmail.com>
2021-10-09 14:36:40 +0800

    add open telemetry support

---
+++ otelfiber/example/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/contrib/otelfiber"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	oteltrace "go.opentelemetry.io/otel/trace"
+)
+
+var tracer = otel.Tracer("fiber-server")
+
+func main() {
+	tp := initTracer()
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	app := fiber.New()
+	app.Use(otelfiber.Middleware("my-server"))
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return errors.New("abc")
+	})
+
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		name := getUser(c.UserContext(), id)
+		return c.JSON(fiber.Map{"id": id, name: name})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func initTracer() *sdktrace.TracerProvider {
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	if err != nil {
+		log.Fatal(err)
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
+
+func getUser(ctx context.Context, id string) string {
+	_, span := tracer.Start(ctx, "getUser", oteltrace.WithAttributes(attribute.String("id", id)))
+	defer span.End()
+	if id == "123" {
+		return "otelfiber tester"
+	}
+	return "unknown"
+}
