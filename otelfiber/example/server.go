470d3a5939a4a63c3534f14a1fecbf757cf6121d
Wei Lun <weilun_95@hotmail.com>
2022-01-14 15:38:02 +0800

    update example

--- otelfiber/example/server.go
+++ otelfiber/example/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,8 +12,10 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	//"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/propagation"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
@@ -27,6 +30,12 @@ func main() {
 	}()
 
 	app := fiber.New()
+
+	// customise span name
+	//app.Use(otelfiber.Middleware("my-server", otelfiber.WithSpanNameFormatter(func(ctx *fiber.Ctx) string {
+	//	return fmt.Sprintf("%s - %s", ctx.Method(), ctx.Route().Path)
+	//})))
+
 	app.Use(otelfiber.Middleware("my-server"))
 
 	app.Get("/error", func(ctx *fiber.Ctx) error {
@@ -44,12 +53,18 @@ func main() {
 
 func initTracer() *sdktrace.TracerProvider {
 	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	//exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint("http://localhost:14268/api/traces")))
 	if err != nil {
 		log.Fatal(err)
 	}
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("my-service"),
+			)),
 	)
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
