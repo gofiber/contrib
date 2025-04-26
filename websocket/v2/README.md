75991ca3a9073656ef2398c3ac70d948665c23c9
Andres Sabini <andres.sabini@gmail.com>
2025-04-26 18:01:38 -0300

    websocket compatibility with fiber v3

---
+++ websocket/v2/README.md
@@ -0,0 +1,151 @@
+---
+id: websocket
+---
+
+# Websocket
+
+![Release](https://img.shields.io/github/v/tag/gofiber/contrib?filter=websocket*)
+[![Discord](https://img.shields.io/discord/704680098577514527?style=flat&label=%F0%9F%92%AC%20discord&color=00ACD7)](https://gofiber.io/discord)
+![Test](https://github.com/gofiber/contrib/workflows/Test%20websocket/badge.svg)
+
+Based on [Fasthttp WebSocket](https://github.com/fasthttp/websocket) for [Fiber](https://github.com/gofiber/fiber) with available `*fiber.Ctx` methods like [Locals](http://docs.gofiber.io/ctx#locals), [Params](http://docs.gofiber.io/ctx#params), [Query](http://docs.gofiber.io/ctx#query) and [Cookies](http://docs.gofiber.io/ctx#cookies).
+
+**Note: Requires Go 1.18 and above**
+
+## Install
+
+```
+go get -u github.com/gofiber/fiber/v2
+go get -u github.com/gofiber/contrib/websocket
+```
+
+## Signatures
+```go
+func New(handler func(*websocket.Conn), config ...websocket.Config) fiber.Handler {
+```
+
+## Config
+
+| Property            | Type                        | Description                                                                                                                   | Default                |
+|:--------------------|:----------------------------|:------------------------------------------------------------------------------------------------------------------------------|:-----------------------|
+| Filter              | `func(*fiber.Ctx) bool`     | Defines a function to skip middleware.                                                                                        | `nil`                  |
+| HandshakeTimeout    | `time.Duration`             | HandshakeTimeout specifies the duration for the handshake to complete.                                                       | `0` (No timeout)       |
+| Subprotocols        | `[]string`                  | Subprotocols specifies the client's requested subprotocols.                                                                   | `nil`                  |
+| Origins             | `[]string`                  | Allowed Origins based on the Origin header. If empty, everything is allowed.                                                  | `nil`                  |
+| ReadBufferSize      | `int`                       | ReadBufferSize specifies the I/O buffer size in bytes for incoming messages.                                                  | `0` (Use default size) |
+| WriteBufferSize     | `int`                       | WriteBufferSize specifies the I/O buffer size in bytes for outgoing messages.                                                 | `0` (Use default size) |
+| WriteBufferPool     | `websocket.BufferPool`      | WriteBufferPool is a pool of buffers for write operations.                                                                     | `nil`                  |
+| EnableCompression   | `bool`                      | EnableCompression specifies if the client should attempt to negotiate per message compression (RFC 7692).                     | `false`                |
+| RecoverHandler      | `func(*websocket.Conn) void` | RecoverHandler is a panic handler function that recovers from panics.                                                         | `defaultRecover`       |
+
+
+## Example
+
+```go
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/contrib/websocket"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		// c.Locals is added to the *websocket.Conn
+		log.Println(c.Locals("allowed"))  // true
+		log.Println(c.Params("id"))       // 123
+		log.Println(c.Query("v"))         // 1.0
+		log.Println(c.Cookies("session")) // ""
+
+		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+
+	}))
+
+	log.Fatal(app.Listen(":3000"))
+	// Access the websocket server: ws://localhost:3000/ws/123?v=1.0
+	// https://www.websocket.org/echo.html
+}
+
+```
+
+## Note with cache middleware
+
+If you get the error `websocket: bad handshake` when using the [cache middleware](https://github.com/gofiber/fiber/tree/master/middleware/cache), please use `config.Next` to skip websocket path.
+
+```go
+app := fiber.New()
+app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return strings.Contains(c.Route().Path, "/ws")
+		},
+}))
+
+app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {}))
+```
+
+## Note with recover middleware
+
+For internal implementation reasons, currently recover middleware is not work with websocket middleware, please use `config.RecoverHandler` to add recover handler to websocket endpoints.
+By default, config `RecoverHandler` is recovers from panic and writes stack trace to stderr, also returns a response that contains panic message in **error** field.
+
+
+```go
+app := fiber.New()
+
+app.Use(cache.New(cache.Config{
+    Next: func(c *fiber.Ctx) bool {
+        return strings.Contains(c.Route().Path, "/ws")
+    },
+}))
+
+cfg := Config{
+    RecoverHandler: func(conn *Conn) {
+        if err := recover(); err != nil {
+            conn.WriteJSON(fiber.Map{"customError": "error occurred"})
+        }
+    },
+}
+
+app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {}, cfg))
+
+
+```
+
+## Note for WebSocket subprotocols
+
+The config `Subprotocols` only helps you negotiate subprotocols and sets a `Sec-Websocket-Protocol` header if it has a suitable subprotocol. For more about negotiates process, check the comment for `Subprotocols` in [fasthttp.Upgrader](https://pkg.go.dev/github.com/fasthttp/websocket#Upgrader) .
+
+All connections will be sent to the handler function no matter whether the subprotocol negotiation is successful or not. You can get the selected subprotocol from `conn.Subprotocol()`. 
+
+If a connection includes the `Sec-Websocket-Protocol` header in the request but the protocol negotiation fails, the browser will immediately disconnect the connection after receiving the upgrade response.
---
+++ websocket/v2/websocket.go
@@ -0,0 +1,342 @@
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://fiber.wiki
+// 📝 Github Repository: https://github.com/gofiber/fiber
+
+package websocket
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+
+	"github.com/fasthttp/websocket"
+	"github.com/gofiber/fiber/v2/utils"
+	"github.com/gofiber/fiber/v3"
+	"github.com/valyala/fasthttp"
+)
+
+// Config ...
+type Config struct {
+	// Filter defines a function to skip middleware.
+	// Optional. Default: nil
+	Filter func(fiber.Ctx) bool
+
+	// HandshakeTimeout specifies the duration for the handshake to complete.
+	HandshakeTimeout time.Duration
+
+	// Subprotocols specifies the client's requested subprotocols.
+	Subprotocols []string
+
+	// Allowed Origin's based on the Origin header, this validate the request origin to
+	// prevent cross-site request forgery. Everything is allowed if left empty.
+	Origins []string
+
+	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
+	// size is zero, then a useful default size is used. The I/O buffer sizes
+	// do not limit the size of the messages that can be sent or received.
+	ReadBufferSize, WriteBufferSize int
+
+	// WriteBufferPool is a pool of buffers for write operations. If the value
+	// is not set, then write buffers are allocated to the connection for the
+	// lifetime of the connection.
+	//
+	// A pool is most useful when the application has a modest volume of writes
+	// across a large number of connections.
+	//
+	// Applications should use a single pool for each unique value of
+	// WriteBufferSize.
+	WriteBufferPool websocket.BufferPool
+
+	// EnableCompression specifies if the client should attempt to negotiate
+	// per message compression (RFC 7692). Setting this value to true does not
+	// guarantee that compression will be supported. Currently only "no context
+	// takeover" modes are supported.
+	EnableCompression bool
+
+	// RecoverHandler is a panic handler function that recovers from panics
+	// Default recover function is used when nil and writes error message in a response field `error`
+	// It prints stack trace to the stderr by default
+	// Optional. Default: defaultRecover
+	RecoverHandler func(*Conn)
+}
+
+func defaultRecover(c *Conn) {
+	if err := recover(); err != nil {
+		_, _ = os.Stderr.WriteString(fmt.Sprintf("panic: %v\n%s\n", err, debug.Stack())) //nolint:errcheck // This will never fail
+		if err := c.WriteJSON(fiber.Map{"error": err}); err != nil {
+			_, _ = os.Stderr.WriteString(fmt.Sprintf("could not write error response: %v\n", err))
+		}
+	}
+}
+
+// New returns a new `handler func(*Conn)` that upgrades a client to the
+// websocket protocol, you can pass an optional config.
+func New(handler func(*Conn), config ...Config) fiber.Handler {
+	// Init config
+	var cfg Config
+	if len(config) > 0 {
+		cfg = config[0]
+	}
+	if len(cfg.Origins) == 0 {
+		cfg.Origins = []string{"*"}
+	}
+	if cfg.ReadBufferSize == 0 {
+		cfg.ReadBufferSize = 1024
+	}
+	if cfg.WriteBufferSize == 0 {
+		cfg.WriteBufferSize = 1024
+	}
+	if cfg.RecoverHandler == nil {
+		cfg.RecoverHandler = defaultRecover
+	}
+	var upgrader = websocket.FastHTTPUpgrader{
+		HandshakeTimeout:  cfg.HandshakeTimeout,
+		Subprotocols:      cfg.Subprotocols,
+		ReadBufferSize:    cfg.ReadBufferSize,
+		WriteBufferSize:   cfg.WriteBufferSize,
+		EnableCompression: cfg.EnableCompression,
+		WriteBufferPool:   cfg.WriteBufferPool,
+		CheckOrigin: func(fctx *fasthttp.RequestCtx) bool {
+			if cfg.Origins[0] == "*" {
+				return true
+			}
+			origin := utils.UnsafeString(fctx.Request.Header.Peek("Origin"))
+			for i := range cfg.Origins {
+				if cfg.Origins[i] == origin {
+					return true
+				}
+			}
+			return false
+		},
+	}
+	return func(c fiber.Ctx) error {
+		if cfg.Filter != nil && !cfg.Filter(c) {
+			return c.Next()
+		}
+
+		conn := acquireConn()
+		// locals
+		c.RequestCtx().VisitUserValues(func(key []byte, value interface{}) {
+			conn.locals[string(key)] = value
+		})
+
+		// params
+		params := c.Route().Params
+		for i := 0; i < len(params); i++ {
+			conn.params[utils.CopyString(params[i])] = utils.CopyString(c.Params(params[i]))
+		}
+
+		// queries
+		c.RequestCtx().QueryArgs().VisitAll(func(key, value []byte) {
+			conn.queries[string(key)] = string(value)
+		})
+
+		// cookies
+		c.RequestCtx().Request.Header.VisitAllCookie(func(key, value []byte) {
+			conn.cookies[string(key)] = string(value)
+		})
+
+		// headers
+		c.RequestCtx().Request.Header.VisitAll(func(key, value []byte) {
+			conn.headers[string(key)] = string(value)
+		})
+
+		// ip address
+		conn.ip = c.IP()
+
+		if err := upgrader.Upgrade(c.RequestCtx(), func(fconn *websocket.Conn) {
+			conn.Conn = fconn
+			defer releaseConn(conn)
+			defer cfg.RecoverHandler(conn)
+			handler(conn)
+		}); err != nil { // Upgrading required
+			return fiber.ErrUpgradeRequired
+		}
+
+		return nil
+	}
+}
+
+// Conn https://godoc.org/github.com/gorilla/websocket#pkg-index
+type Conn struct {
+	*websocket.Conn
+	locals  map[string]interface{}
+	params  map[string]string
+	cookies map[string]string
+	headers map[string]string
+	queries map[string]string
+	ip      string
+}
+
+// Conn pool
+var poolConn = sync.Pool{
+	New: func() interface{} {
+		return new(Conn)
+	},
+}
+
+// Acquire Conn from pool
+func acquireConn() *Conn {
+	conn := poolConn.Get().(*Conn)
+	conn.locals = make(map[string]interface{})
+	conn.params = make(map[string]string)
+	conn.queries = make(map[string]string)
+	conn.cookies = make(map[string]string)
+	conn.headers = make(map[string]string)
+	return conn
+}
+
+// Return Conn to pool
+func releaseConn(conn *Conn) {
+	conn.Conn = nil
+	poolConn.Put(conn)
+}
+
+// Locals makes it possible to pass interface{} values under string keys scoped to the request
+// and therefore available to all following routes that match the request.
+func (conn *Conn) Locals(key string, value ...interface{}) interface{} {
+	if len(value) == 0 {
+		return conn.locals[key]
+	}
+	conn.locals[key] = value[0]
+	return value[0]
+}
+
+// Params is used to get the route parameters.
+// Defaults to empty string "" if the param doesn't exist.
+// If a default value is given, it will return that value if the param doesn't exist.
+func (conn *Conn) Params(key string, defaultValue ...string) string {
+	v, ok := conn.params[key]
+	if !ok && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return v
+}
+
+// Query returns the query string parameter in the url.
+// Defaults to empty string "" if the query doesn't exist.
+// If a default value is given, it will return that value if the query doesn't exist.
+func (conn *Conn) Query(key string, defaultValue ...string) string {
+	v, ok := conn.queries[key]
+	if !ok && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return v
+}
+
+// Cookies is used for getting a cookie value by key
+// Defaults to empty string "" if the cookie doesn't exist.
+// If a default value is given, it will return that value if the cookie doesn't exist.
+func (conn *Conn) Cookies(key string, defaultValue ...string) string {
+	v, ok := conn.cookies[key]
+	if !ok && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return v
+}
+
+// Headers is used for getting a header value by key
+// Defaults to empty string "" if the header doesn't exist.
+// If a default value is given, it will return that value if the header doesn't exist.
+func (conn *Conn) Headers(key string, defaultValue ...string) string {
+	v, ok := conn.headers[key]
+	if !ok && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return v
+}
+
+// IP returns the client's network address
+func (conn *Conn) IP() string {
+	return conn.ip
+}
+
+// Constants are taken from https://github.com/fasthttp/websocket/blob/master/conn.go#L43
+
+// Close codes defined in RFC 6455, section 11.7.
+const (
+	CloseNormalClosure           = 1000
+	CloseGoingAway               = 1001
+	CloseProtocolError           = 1002
+	CloseUnsupportedData         = 1003
+	CloseNoStatusReceived        = 1005
+	CloseAbnormalClosure         = 1006
+	CloseInvalidFramePayloadData = 1007
+	ClosePolicyViolation         = 1008
+	CloseMessageTooBig           = 1009
+	CloseMandatoryExtension      = 1010
+	CloseInternalServerErr       = 1011
+	CloseServiceRestart          = 1012
+	CloseTryAgainLater           = 1013
+	CloseTLSHandshake            = 1015
+)
+
+// The message types are defined in RFC 6455, section 11.8.
+const (
+	// TextMessage denotes a text data message. The text message payload is
+	// interpreted as UTF-8 encoded text data.
+	TextMessage = 1
+
+	// BinaryMessage denotes a binary data message.
+	BinaryMessage = 2
+
+	// CloseMessage denotes a close control message. The optional message
+	// payload contains a numeric code and text. Use the FormatCloseMessage
+	// function to format a close message payload.
+	CloseMessage = 8
+
+	// PingMessage denotes a ping control message. The optional message payload
+	// is UTF-8 encoded text.
+	PingMessage = 9
+
+	// PongMessage denotes a pong control message. The optional message payload
+	// is UTF-8 encoded text.
+	PongMessage = 10
+)
+
+var (
+	// ErrBadHandshake is returned when the server response to opening handshake is
+	// invalid.
+	ErrBadHandshake = errors.New("websocket: bad handshake")
+	// ErrCloseSent is returned when the application writes a message to the
+	// connection after sending a close message.
+	ErrCloseSent = errors.New("websocket: close sent")
+	// ErrReadLimit is returned when reading a message that is larger than the
+	// read limit set for the connection.
+	ErrReadLimit = errors.New("websocket: read limit exceeded")
+)
+
+// FormatCloseMessage formats closeCode and text as a WebSocket close message.
+// An empty message is returned for code CloseNoStatusReceived.
+func FormatCloseMessage(closeCode int, text string) []byte {
+	return websocket.FormatCloseMessage(closeCode, text)
+}
+
+// IsCloseError returns boolean indicating whether the error is a *CloseError
+// with one of the specified codes.
+func IsCloseError(err error, codes ...int) bool {
+	return websocket.IsCloseError(err, codes...)
+}
+
+// IsUnexpectedCloseError returns boolean indicating whether the error is a
+// *CloseError with a code not in the list of expected codes.
+func IsUnexpectedCloseError(err error, expectedCodes ...int) bool {
+	return websocket.IsUnexpectedCloseError(err, expectedCodes...)
+}
+
+// IsWebSocketUpgrade returns true if the client requested upgrade to the
+// WebSocket protocol.
+func IsWebSocketUpgrade(c fiber.Ctx) bool {
+	return websocket.FastHTTPIsWebSocketUpgrade(c.RequestCtx())
+}
+
+// JoinMessages concatenates received messages to create a single io.Reader.
+// The string term is appended to each message. The returned reader does not
+// support concurrent calls to the Read method.
+func JoinMessages(c *websocket.Conn, term string) io.Reader {
+	return websocket.JoinMessages(c, term)
+}
