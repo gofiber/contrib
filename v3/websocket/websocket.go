4eddbecdfc3db17b2f0424964fa0e2fd56173ce0
René <rene@gofiber.io>
2026-04-11 21:48:38 +0200

    fix: ensure safe copy of IP address in websocket connection

--- v3/websocket/websocket.go
+++ v3/websocket/websocket.go
@@ -72,11 +72,11 @@ type Config struct {
 
 func defaultRecover(c *Conn) {
 	if err := recover(); err != nil {
-                _, _ = fmt.Fprintf(os.Stderr, "panic: %v\n%s\n", err, debug.Stack()) //nolint:errcheck // This will never fail
-                if err := c.WriteJSON(fiber.Map{"error": err}); err != nil {
-                        _, _ = fmt.Fprintf(os.Stderr, "could not write error response: %v\n", err)
-                }
-        }
+		_, _ = fmt.Fprintf(os.Stderr, "panic: %v\n%s\n", err, debug.Stack()) //nolint:errcheck // This will never fail
+		if err := c.WriteJSON(fiber.Map{"error": err}); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "could not write error response: %v\n", err)
+		}
+	}
 }
 
 // New returns a new `handler func(*Conn)` that upgrades a client to the
@@ -173,7 +173,7 @@ func New(handler func(*Conn), config ...Config) fiber.Handler {
 		}
 
 		// ip address
-		conn.ip = c.IP()
+		conn.ip = utils.CopyString(c.IP())
 
 		if err := upgrader.Upgrade(c.RequestCtx(), func(fconn *websocket.Conn) {
 			conn.Conn = fconn
